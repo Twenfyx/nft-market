a54c3025b3bde2cb129c6d86bd835ae418cc4c6f
Twenfyx <senchik549@gmail.com>
2024-02-26 18:15:04 +0100

    add:hero component

---
+++ src/components/BlockchainsSupport.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const BlockchainsSupport = () => {
+  return (
+    <div className="text-center text-4xl mt-5 overflow-hidden relative">
+      <p>Supports 7 blockchains</p>
+      <div className="flex justify-evenly">
+        <p >Ethereum</p>
+        <p >Binance</p>
+        <p >Polygon</p>
+        <p >Arbitron</p>
+        <p >Hedera</p>
+      </div>
+
+      <div className="flex justify-evenly">
+        <p >Ethereum</p>
+        <p >Binance</p>
+        <p >Polygon</p>
+        <p >Arbitron</p>
+        <p >Hedera</p>
+      </div>
+      
+    </div>
+  );
+};
+
+export default BlockchainsSupport;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,26 @@
+// import main from '../assets/hero/main.jpg'
+import ape from '../assets/hero/ape.gif'
+import BlockchainsSupport from './BlockchainsSupport'
+const Hero = () => {
+  return (
+    <div className="h-screen bg-black">
+      <div className='flex justify-evenly text-8xl'>
+        <img src={ape} alt="" className='' />
+        <div className="flex justify-center items-center ">
+          <p className='text-white font-mono'>
+            Welcome 
+          To <br /> Digital <br />
+          <span className="bg-clip-text text-transparent animate-wave-gradient">World</span>
+
+
+           </p>
+        </div>
+
+      </div>
+    <BlockchainsSupport />
+
+    </div>
+  )
+}
+
+export default Hero
